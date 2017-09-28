825e6379ad91f1169c04e45190c222d3fe512496
Peter Eriksson <96peerik@gmail.com>
2017-09-28 12:41:54 +0200

    added leveldb persistence

--- cached-downloader.js
+++ cached-downloader.js
@@ -2,46 +2,56 @@ const EventEmitter = require('events').EventEmitter;
 const Downloader = require('./lib/downloader');
 const Cache = require('./lib/referenced-file-cache');
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
-
-function getTempFolder() {
-  return path.join(os.tmpdir(), 'cached-downloader'); 
-}
-
-class ProgressItem extends EventEmitter {
-  constructor(promise) {
-    this.promise = promise;
-    this.totalBytes;
-    this.downloadedBytes;
-  }
-}
+const uuid = require('uuid');
+const crypto = require('crypto');
 
 class CachedDownloader extends EventEmitter {
   constructor(options) {
     super();
-    this.options = options || {};
-    this.localDirectory = this.options.localDirectory || getTempFolder()
+    if ((options == null) || (options.localDirectory == null)) {
+      throw(new Error('CachedDownloader: localDirectory property missing in options object to constructor'));
+    }
+    this.localDirectory = options.localDirectory;
     this.progress = new Map();
-    this.cache = new Cache();
-    this.cache.on('remove', (item) => console.log('remove', item));
+    this.cache = new Cache({ path: this.localDirectory });
+    this.cache.on('remove', (item) => {
+      fs.unlink(item.filename, (err) => console.error(err)); 
+    });
   }
 
-  download(url, ref) {
+  init() {
+    return this.cache.init();
+  }
+
+  static hashString(str) {
+    return crypto.createHash('md5').update(str).digest('hex');    
+  }
+
+  download(url, localFile, ref) {
+    const filename = localFile || CachedDownloader.hashString(url);
     // download of this url already in progress - return original promise
     if (this.progress.has(url)) return this.progress.get(url);
-    
-    const promise = this.cache.get(url, ref).then(item => item.filename)
-      .catch((err) => Downloader.download(url, path.join(this.localDirectory, 'test.txt'))
-        .then((item) => this.cache.set(url, item.filename, ref))
-        .then((item) => {
-          this.progress.delete(url);
-          return item.filename
-        })
-        .catch((err) => {
-          this.progress.delete(url);
-          throw err;
-        })
-      )
+    const promise = this.cache.get(url, ref).then((item) => {
+      this.progress.delete(url);
+      return item.filename;
+    })
+    .catch((err) => {
+      return Downloader.download(url, path.join(this.localDirectory, filename), true, (bytes, total) => {
+       console.log(bytes, total); 
+      })
+      .then((item) => this.cache.set(url, item.filename, ref))
+      .then((item) => {
+        this.progress.delete(url);
+        return item.filename
+      })
+      .catch((err) => {
+        this.progress.delete(url);
+        throw err;
+      })
+    });
+      
     this.progress.set(url, promise);
     return promise;
   }
--- testing.js
+++ testing.js
@@ -1,17 +1,40 @@
 const Downloader = require('./cached-downloader');
+const os = require('os');
+const path = require('path');
+const dir = '/Users/peter/filecache';
 
-const dl = new Downloader();
+const dl = new Downloader({ localDirectory: dir });
+dl.init().then(() => {
+})
+.catch((err) => console.error(err));
 
-dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', 'bongos')
-.then((filename) => console.log(filename))
-.catch(err => console.error(err));
+setTimeout(() => {
+    dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', null, 'bongos')
+    .then((filename) => console.log(filename))
+    .catch(err => console.error(err));
+    
+    dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', null, 'bongos')
+    .then((filename) => console.log(filename))
+    .catch(err => console.error(err));
+    
+    dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', null, 'bongos')
+    .then((filename) => console.log(filename))
+    .catch(err => console.error(err));
+}, 1000);
 
-dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', 'bongos')
-.then((filename) => console.log(filename))
-.catch(err => console.error(err));
+setTimeout(() => {
+    dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', null, 'bongos')
+    .then((filename) => console.log(filename))
+    .catch(err => console.error(err));
+    
+    dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', null, 'bongos')
+    .then((filename) => console.log(filename))
+    .catch(err => console.error(err));
+    
+    dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', null, 'bongos')
+    .then((filename) => console.log(filename))
+    .catch(err => console.error(err));
+}, 10000);
 
-dl.download('https://www.thomann.de/pics/bdb/147236/10085220_800.jpg', 'bongos')
-.then((filename) => console.log(filename))
-.catch(err => console.error(err));
 
-setTimeout(() => dl.clearRef('bongos'), 5000);
+//setTimeout(() => dl.clearRef('bongos'), 5000);
